be8e0be7ed5f50e3ba68250689cda7291ecf52c6
ilovapples <89090508+ilovapples@users.noreply.github.com>
2023-02-13 08:35:45 -0600

    Update script.js

--- script.js
+++ script.js
@@ -12,11 +12,29 @@ var color = 0
 function colormode() {
   if (color === 0) {
     var html = document.getElementById("html");
+    var title = document.getElementById("title");
+    var subtitle = document.getElementById("subtitle");
+    var linkslabel = document.getElementById("linkslabel");
+    var colormodeimg = document.getElementById("colormodeimg");
+
     html.setAttribute("style", "background-color: #ffffff"); 
+    title.setAttribute("style", "color: #000000");
+    subtitle.setAttribute("style", "color: #000000");
+    linkslabel.setAttribute("style", "color: #000000");
+    colormodeimg.setAttribute("src", "imgs/colormodes-dark.png")
     color = 1
   } else if (color === 1) {
     var html = document.getElementById("html");
-    html.setAttribute("style", "background-color: #2d2d2d");
+    var title = document.getElementById("title");
+    var subtitle = document.getElementById("subtitle");
+    var linkslabel = document.getElementById("linkslabel");
+    var colormodeimg = document.getElementById("colormodeimg");
+
+    html.setAttribute("style", "background-color: #2d2d2d"); 
+    title.setAttribute("style", "color: #ffffff");
+    subtitle.setAttribute("style", "color: #ffffff");
+    linkslabel.setAttribute("style", "color: #ffffff");
+    colormodeimg.setAttribute("src", "imgs/colormodes-light.png")
     color = 0
   }
 }
