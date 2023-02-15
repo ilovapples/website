72aff21731b4b6fc5f6b630adc4488accb011cf2
ilovapples <89090508+ilovapples@users.noreply.github.com>
2023-02-15 14:08:35 -0600

    Update script.js

--- datapacks/script.js
+++ datapacks/script.js
@@ -1,3 +1,13 @@
-function float_blocks() {
+function float_blocks(platform) {
+  if (platform === 'planetminecraft') {
+    window.open("https://planetminecraft.com/data-pack/float-blocks/")
+  } else if (platform === 'modrinth' {
+    window.open("https://modrinth.com/datapack/float-blocks/")
+  }
+}
+function float_blocks__planetminecraft() {
   window.open("https://planetminecraft.com/data-pack/float-blocks/")
+}
+function float_blocks__modrinth() {
+  window.open("https://modrinth.com/datapack/float-blocks/")
 }
